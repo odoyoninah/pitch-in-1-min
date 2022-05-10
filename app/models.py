fa61e6097fe42269c876f1ebcf362bdee9a56ff8
odoyonina <odoyoninah@gmail.com>
2022-05-10 11:56:46 +0300

    create email file

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
