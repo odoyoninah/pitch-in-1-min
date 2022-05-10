4ae2a5c7cae8aeb8e6fbc4ecb891d06f962b9ef0
odoyonina <odoyoninah@gmail.com>
2022-05-10 03:04:15 +0300

    define class role

--- app/models.py
+++ app/models.py
@@ -1,2 +1,9 @@
 from . import db
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
