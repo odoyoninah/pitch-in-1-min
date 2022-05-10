3090cf7c79d6146296ae897a3ea39ea1d8b3c6d2
odoyonina <odoyoninah@gmail.com>
2022-05-10 10:57:03 +0300

    add

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,13 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
 
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(), Email()])
+    username = StringField('Enter your username', validators=[Required(), Length(min=4, max=25)])
+    password = PasswordField('Password', validators=[Required(), Length(min=4, max=25)])
+    confirm_password = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
+    EqualTo('password2') ,message = 'Passwords must match'
+    password2 = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    
