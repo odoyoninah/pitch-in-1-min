e2d2d2ea59fe33a2b84d0626615dfdb8ba2ad9cf
odoyonina <odoyoninah@gmail.com>
2022-05-10 04:57:39 +0300

    create main folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
+from ensurepip import bootstrap
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    db.init_app(app)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
