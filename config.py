ff88517e0332a5e724ad627b8ea6580aad200e1d
odoyonina <odoyoninah@gmail.com>
2022-05-10 03:38:48 +0300

    add config options

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
+from distutils.command.config import config
+
+
 class Config:
     pass
 
 class ProdConfig(Config):
     pass
 
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
