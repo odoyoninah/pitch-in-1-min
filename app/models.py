c386991e69782002eb90880bc96f36e90091215e
odoyonina <odoyoninah@gmail.com>
2022-05-10 03:08:48 +0300

    create user class

--- app/models.py
+++ app/models.py
@@ -7,3 +7,15 @@ class Role(db.Model):
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
+        return f'User (self.name)'
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+    def __repr__(self):
+        return f'User (self.username)'
+        
