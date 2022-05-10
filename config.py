317008a3e16743bc00d262070c8f33696049ebdd
odoyonina <odoyoninah@gmail.com>
2022-05-10 03:55:59 +0300

    create class test

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
 from distutils.command.config import config
 
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProdConfig(Config):
     pass
